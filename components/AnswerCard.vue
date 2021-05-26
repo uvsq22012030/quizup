c28df849284e0c39cbff2660c58659851be368f0
Dylan Tientcheu <dylantientcheu@gmail.com>
2021-05-26 15:03:26 +0200

    fix sk answer card bug

--- components/AnswerCard.vue
+++ components/AnswerCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center w-full p-2 my-3 transition ease-out transform scale-95 rounded-lg shadow-xl cursor-pointer h-15 md:h-50 md:w-1/2 hover:bg-indigo-600 hover:scale-100 border-1 hover:border-bg-indigo-400 hover:shadow-2xl"
+    class="flex items-center justify-center w-full p-2 my-3 transition ease-out transform scale-95 rounded-lg shadow-xl cursor-pointer h-15 md:h-30 md:w-1/2 hover:bg-indigo-600 hover:scale-100 border-1 hover:border-bg-indigo-400 hover:shadow-2xl"
     :class="{
       'bg-green-600 hover:bg-green-600 hover:scale-100 hover:border-bg-green-600': isCorrect,
       'bg-red-600 hover:bg-red-600 hover:scale-100 hover:border-bg-red-600': isFalse,
